e4eb4a489f4d532188402fdd7de18e03c9e7031c
apurvamagar15 <159618512+apurvamagar15@users.noreply.github.com>
2024-09-24 16:18:44 +0530

    Create slip26.java

---
+++ slip26.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class slip26
+{
+public static void main(String arg[])
+{
+char ch1='a';
+char ch2='b';
+char ch3='B';
+int asciivalue1=ch1;
+int asciivalue2=ch2;
+int asciivalue3=ch3;
+System.out.println("the ascii value of"+ch1+"value :"+asciivalue1);
+System.out.println("the ascii value of"+ch2+"value :"+asciivalue2);
+System.out.println("the ascii value of"+ch3+"value :"+asciivalue3);
+}}
